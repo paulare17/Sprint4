e15deb09187de761039e5fd0793ab8a9e0af6eaa
Paula <paulare17@gmail.com>
2025-03-19 17:33:37 +0100

    Repensat primer exercici

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,25 @@
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const button: HTMLElement | null = document.getElementById("button");
+let button: HTMLElement | null = document.getElementById("button");
 let jokes: HTMLElement | null = document.getElementById("jokes-container");
 
-// const callJoke = () => {
-//   fetch("https://icanhazdadjoke.com/")
-//     .then((resposta) => console.log(resposta.json()))
-//     .then((data) => {
-//       console.log(data)
-//     //   showJoke(data)
 
-//   }
-// )
-// };
-
-// callJoke();
-
-// function showJoke(joke) {
-//   const acudit = document.createElement('acudit')
-//   console.log(joke)
-//   jokes.innerHTML = data.joke;
-// }
-
-async function takeJokes() {
-const acudits = await fetch("https://icanhazdadjoke.com/", {
-  headers: {
-    Accept: "application/json"
-  }
-})
-.then(res => res.json())
-.then(data => {
-  console.log(data.joke)
-  return data.joke
-})
-
-jokes.innerHTML = acudits
-
+function takeJokes() {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.joke);
+      const acudit = data.joke;
+      jokes.innerHTML = acudit;
+    });
 }
 
-button.addEventListener('click', takeJokes)
+takeJokes();
+button.addEventListener("click", takeJokes);
+
