2d02a1569f4ba9154348fb1776c81a4ded50e724
Paula <paulare17@gmail.com>
2025-03-23 17:39:40 +0100

    Exercici 5 finalitzat: implementat acudits de Chuck Norris

--- src/apigestor.ts
+++ src/apigestor.ts
@@ -1,30 +1,43 @@
 
+//API principal
 
 export async function takeJokes() {
-    return fetch("https://icanhazdadjoke.com/", {
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => data.joke);
-    }
-    
-
-
-const showWeather: HTMLElement | null = document.getElementById("weather")
-
-
-   export async function obtainWeather() {
-      const resposta = await fetch(`https://api.weatherapi.com/v1/current.json?key=9abcfa56cc7e4cd885c131129252303&q=Barcelona`);
-      const dades = await resposta.json();
-      
-      console.log(dades); 
-      if (showWeather)
-      showWeather.innerHTML = `La temperatura a Barcelona és ${dades.current.temp_c}ºC i la humitat és del ${dades.current.humidity}%. <br>
-    ${dades.current.condition.text}.`
-      
+  return fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data.joke);
 }
- 
 
-  
+//API meteorologia
+
+const showWeather: HTMLElement | null = document.getElementById("weather");
+
+export async function obtainWeather() {
+  const resposta = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=9abcfa56cc7e4cd885c131129252303&q=Barcelona`
+  );
+  const dades = await resposta.json();
+
+  console.log(dades);
+  if (showWeather)
+    showWeather.innerHTML = `La temperatura a Barcelona és ${dades.current.temp_c}ºC i la humitat és del ${dades.current.humidity}%. <br>
+    ${dades.current.condition.text}.`;
+}
+
+
+//API chuck norris
+
+export async function takeJokesChuck() {
+  return fetch("https://api.chucknorris.io/jokes/random", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+  .then((res) => res.json())
+  .then((data) => data.value);
+
+}
+
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { takeJokes } from "./apigestor";
 import { obtainWeather } from "./apigestor";
+import { takeJokesChuck } from "./apigestor";
 
 const button: HTMLElement | null = document.getElementById("button");
 let jokes: HTMLElement | null = document.getElementById("jokes-container");
@@ -12,21 +13,27 @@ const score1: HTMLElement | null = document.getElementById("score1");
 const score2: HTMLElement | null = document.getElementById("score2");
 const score3: HTMLElement | null = document.getElementById("score3");
 
+let apiAltern = 0;
+const apis = [takeJokes, takeJokesChuck];
+
 async function captureJokes() {
   const time = new Date().toISOString();
-  const acudit = await takeJokes();
+  const acudit = await apis[apiAltern]();;
+
   reportJokes.push({ joke: acudit, score: null, date: time });
   console.log(reportJokes);
+
+  apiAltern = (apiAltern + 1) % apis.length;
 }
 
 async function printJokes() {
-  const printAcudit = await takeJokes();
+  const printAcudit = await apis[apiAltern]();;
   console.log(printAcudit);
   if (jokes) jokes.innerHTML = printAcudit;
 }
 
 async function scoreJokes(puntuacio: number) {
-  await takeJokes();
+  await await apis[apiAltern]();;
 
   reportJokes[reportJokes.length - 1].score = puntuacio;
   console.log(reportJokes);
@@ -40,25 +47,18 @@ window.addEventListener("load", () => {
   obtainWeather();
 });
 
-button.addEventListener("click", () => {
+button?.addEventListener("click", () => {
   takeJokes();
   captureJokes();
   printJokes();
 });
 
-score1.addEventListener("click", () => {
+score1?.addEventListener("click", () => {
   scoreJokes(1);
 });
-score2.addEventListener("click", () => {
+score2?.addEventListener("click", () => {
   scoreJokes(2);
 });
-score3.addEventListener("click", () => {
+score3?.addEventListener("click", () => {
   scoreJokes(3);
 });
-
-/* API de xistes a contemplar:
-https://sv443.net/jokeapi/v2/
-https://api.chucknorris.io 
-
-
-*/
